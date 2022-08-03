209d379ed9b17c3a0dba6bef23e0356de22fdd15
Redstoneur <alfa.aog36@gmail.com>
2022-08-03 14:44:21 +0200

    Version 0.2.5

--- Utilities/Tools/Languages.py
+++ Utilities/Tools/Languages.py
@@ -1,14 +1,8 @@
 import locale
 
-# import googletrans
-# from googletrans import Translator
-
-
 ######################################################################################################################
 ############################## Functions Translator ##################################################################
 ######################################################################################################################
-
-# listOfLanguages: list = list(googletrans.LANGUAGES.keys())
 
 listOfLanguages: list = ['fr', 'en']
 
@@ -48,37 +42,5 @@ def haveLanguage(language: str = "?") -> str:
 #     :param language_of_translation: str, language of the translation
 #     :return: str, translated text
 #     """
-#     text_translated = Translator().translate(text, dest=language_of_translation)
-#     print(text_translated.text)
-#     translator = Translator()
-#     if language_of_text == language_of_translation:
-#         return text
-#     elif (language_of_text not in listOfLanguages and language_of_text is not None) \
-#             or language_of_translation not in listOfLanguages:
-#         return None
-#     else:
-#         try:
-#             print(translator.translate(['bonjour'], dest=language_of_translation).text)
-#             return translator.translate(text, dest=language_of_translation).text
-#         except Exception as e:
-#             print(e)
-#             return None
 #
 # if __name__ == "__main__":
-#     #     txt_fr = "Bonjour"
-#     #     txt_en = "Hello"
-#     #
-#     #     translator = Translator()
-#     #
-#     #     translate = translator.translate(txt_fr, src="fr", dest="en")
-#     #     print(translate)
-#     #
-#     #     print("the text '"+txt_fr+"' is in the language '"+translator.detect(txt_fr)+"'")
-#     #     print("the text '"+txt_en+"' is in the language '"+translator.detect(txt_en)+"'")
-#     #
-#     #     print("the text '"+txt_fr+"' is translated in the language '"+translator.detect(translator.translate(text=txt_fr, src="fr", dest="en").text)+"'")
-#     #     print("the text '"+txt_en+"' is translated in the language '"+translator.detect(translator.translate(text=txt_en, src="en", dest="fr").text)+"'")
-#     #
-#     #     print(Translator.detect("Bonjour"))
-#     print(translateSimple("Bonjour", "fr", "en"))
-#     # print(translateSimple("Hello", "en", "fr"))
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='Python_Utilities',
-      version='0.2.4',
+      version='0.2.5',
       description='A simple test package',
       long_description=open('README.md').read(),
       url='https://github.com/Redstoneur/Python-Utilities',
